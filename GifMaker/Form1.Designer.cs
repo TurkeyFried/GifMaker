7cc3f02cf3744d6cc990e0d64989c6e0c0676dec
Luis Pabon <turkeyfried@gmail.com>
2015-02-07 20:25:22 -0500

    Initial Commit

---
+++ GifMaker/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GifMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.previewButton = new System.Windows.Forms.Button();
+            this.imageContainer = new System.Windows.Forms.Panel();
+            this.imageLibrary = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(293, 438);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(98, 33);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(443, 421);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(135, 50);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Add Images";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // previewBox
+            // 
+            this.previewBox.Location = new System.Drawing.Point(12, 214);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(275, 257);
+            this.previewBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.previewBox.TabIndex = 3;
+            this.previewBox.TabStop = false;
+            // 
+            // previewButton
+            // 
+            this.previewButton.Location = new System.Drawing.Point(293, 355);
+            this.previewButton.Name = "previewButton";
+            this.previewButton.Size = new System.Drawing.Size(98, 33);
+            this.previewButton.TabIndex = 5;
+            this.previewButton.Text = "Preview";
+            this.previewButton.UseVisualStyleBackColor = true;
+            // 
+            // imageContainer
+            // 
+            this.imageContainer.AllowDrop = true;
+            this.imageContainer.AutoScroll = true;
+            this.imageContainer.BackColor = System.Drawing.SystemColors.Window;
+            this.imageContainer.Location = new System.Drawing.Point(12, 12);
+            this.imageContainer.Name = "imageContainer";
+            this.imageContainer.Size = new System.Drawing.Size(535, 100);
+            this.imageContainer.TabIndex = 6;
+            this.imageContainer.DragDrop += new System.Windows.Forms.DragEventHandler(this.imageContainer_DragDrop);
+            // 
+            // imageLibrary
+            // 
+            this.imageLibrary.AutoScroll = true;
+            this.imageLibrary.BackColor = System.Drawing.SystemColors.Window;
+            this.imageLibrary.Location = new System.Drawing.Point(584, 12);
+            this.imageLibrary.Name = "imageLibrary";
+            this.imageLibrary.Size = new System.Drawing.Size(300, 459);
+            this.imageLibrary.TabIndex = 7;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(293, 214);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 483);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.imageLibrary);
+            this.Controls.Add(this.imageContainer);
+            this.Controls.Add(this.previewButton);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.PictureBox previewBox;
+        private System.Windows.Forms.Button previewButton;
+        private System.Windows.Forms.Panel imageContainer;
+        private System.Windows.Forms.Panel imageLibrary;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ GifMaker/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ImageMagick;
+
+namespace GifMaker
+{
+    public partial class Form1 : Form
+    {
+        private const int thumbnailX = 100;
+        private const int thumbnailY = 100;
+
+        private int currentColumn = 0;
+        private int currentRow = 0;
+
+        private PictureBox activeControl;
+        private Point previousLocation;
+
+        private MagickImageCollection libraryList;
+        private MagickImageCollection workList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            activeControl = null;
+            libraryList = new MagickImageCollection();
+            workList = new MagickImageCollection();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveImage();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            loadImage();
+        }
+
+        private void imageContainer_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.Write("imageContainer_DragDrop");
+        }
+
+        void libraryPicture_MouseDown(object sender, MouseEventArgs e)
+        {
+            activeControl = sender as PictureBox;
+
+            PictureBox newPicture = new PictureBox();
+            newPicture.Image = activeControl.Image;
+            newPicture.SizeMode = activeControl.SizeMode;
+            newPicture.BorderStyle = activeControl.BorderStyle;
+            newPicture.Size = activeControl.Size;
+            newPicture.Location = activeControl.Location;
+            newPicture.BringToFront();
+            imageLibrary.Controls.Add(newPicture);
+
+            activeControl.BringToFront();
+
+            // http://stackoverflow.com/a/3870225/1978219
+            newPicture.MouseDown += new MouseEventHandler(libraryPicture_MouseDown);
+            newPicture.MouseMove += new MouseEventHandler(libraryPicture_MouseMove);
+            newPicture.MouseUp += new MouseEventHandler(libraryPicture_MouseUp);
+
+            this.Controls.Add(activeControl);
+
+            previousLocation = e.Location;
+            Cursor = Cursors.Hand;
+        }
+
+        void libraryPicture_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (activeControl == null || activeControl.Image == null)
+            {
+                return;
+            }
+
+            var location = activeControl.Location;
+            location.Offset(e.Location.X - previousLocation.X, e.Location.Y - previousLocation.Y);
+            activeControl.Location = location;
+        }
+
+        void libraryPicture_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (activeControl == null || activeControl.Image == null)
+            {
+                return;
+            }
+            activeControl.Dispose();
+            Cursor = Cursors.Default;
+        }
+
+        private void loadImage()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in fileDialog.FileNames)
+                {
+                    // http://stackoverflow.com/a/17736176/1978219
+                    if (fileName.EndsWith(".gif"))
+                    {
+                        MagickImageCollection frames = new MagickImageCollection(fileName);
+
+                        foreach (MagickImage frame in frames)
+                        {
+                            addToLibrary(frame);
+                        }
+
+                        continue;
+                    }
+
+                    addToLibrary(new MagickImage(fileName));
+                }
+            }
+        }
+
+        private void addToLibrary(MagickImage fileImage)
+        {
+            if (libraryList.Contains(fileImage))
+            {
+                return;
+            }
+
+            PictureBox newPicture = new PictureBox();
+            newPicture.Image = fileImage.ToBitmap();
+            newPicture.SizeMode = PictureBoxSizeMode.Zoom;
+            newPicture.BorderStyle = BorderStyle.FixedSingle;
+            newPicture.Size = new Size(thumbnailX, thumbnailY);
+
+            newPicture.Location = new Point(thumbnailX * currentColumn, thumbnailY * currentRow);
+
+            if (currentColumn >= 2)
+            {
+                currentColumn = 0;
+                currentRow++;
+            }
+            else
+            {
+                currentColumn++;
+            }
+
+            imageLibrary.Controls.Add(newPicture);
+
+            newPicture.BringToFront();
+
+            // http://stackoverflow.com/a/3870225/1978219
+            newPicture.MouseDown += new MouseEventHandler(libraryPicture_MouseDown);
+            newPicture.MouseMove += new MouseEventHandler(libraryPicture_MouseMove);
+            newPicture.MouseUp += new MouseEventHandler(libraryPicture_MouseUp);
+
+            //fileImage.AnimationDelay = 300;
+            libraryList.Add(fileImage);
+
+            //MagickImageCollection previewGif = createGif(libraryList);
+            previewBox.Image = libraryList.ToBitmap(ImageFormat.Gif);
+        }
+
+        // https://magick.codeplex.com/wikipage?title=Combining%20images&referringTitle=Documentation
+        private MagickImageCollection createGif(MagickImageCollection collection)
+        {
+
+            //foreach (MagickImage image in collection)
+            //{
+            //    image.AnimationDelay = 3;
+            //}
+            //collection[0].AnimationDelay = 300;
+
+            // Optionally optimize the images (images should have the same size).
+            //collection.Optimize();
+
+            return collection;
+        }
+
+        private void saveImage()
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "GIF Images|*.gif";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+
+
+                foreach (MagickImage image in libraryList)
+                {
+                    image.AnimationDelay = 3;
+                }
+
+                libraryList.Write(saveDialog.FileName);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown thing = sender as NumericUpDown;
+
+            foreach (MagickImage image in libraryList)
+            {
+                image.AnimationDelay = (int)thing.Value;
+            }
+
+            previewBox.Image = libraryList.ToBitmap(ImageFormat.Gif);
+        }
+    }
+}
